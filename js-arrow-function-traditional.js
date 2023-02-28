2358d527bb1c0d5348df0dc3b7c2aca1e0354e28
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-01 10:02:20 +1100

    Arrow function notes

--- js-arrow-function-traditional.js
+++ js-arrow-function-traditional.js
@@ -6,17 +6,18 @@ function showDuncan(a) {
 
 // 1. Change the word function to const
 const showDuncan(parameter) {
-  console.log("Hi, I'm Duncan 🐾!")
+  console.log("Hi, I'm Duncan 🐾!");
   return parameter + 1;
 }
 
 // 2. Add "=" between the function name and argument
 const showDuncan = (parameter) {
-  console.log("Hi, I'm Duncan 🐾!")
+  console.log("Hi, I'm Duncan 🐾!");
   return parameter + 1;
 }
 
 // 3. Add the arrow sign between the argument and the opening curly brace 
-const showDuncan = (parameter) => {console.log("Hi, I'm Duncan 🐾!")
-return parameter + 1;
+const showDuncan = (parameter) => {
+  console.log("Hi, I'm Duncan 🐾!");
+  return parameter + 1;
 }
