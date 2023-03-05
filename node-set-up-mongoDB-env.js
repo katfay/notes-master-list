ba3ae2715aed1fdddfabdb3830b591e749ea44ed
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-05 16:27:38 +1100

    Create node-set-up-mongoDB-env.js

---
+++ node-set-up-mongoDB-env.js
@@ -0,0 +1,15 @@
+// Check node, npm and nodemon are installed locally.
+
+// Initialise a JSON package (npm init).
+
+// Install these modules: dotenv, cors, body-parser, express, mongoose (npm install <module> --save).
+
+// Install npm again (npm install).
+
+// In JSON file, add "start" script (under "test" script). Value is "nodemon index.js" (or apporopriate filepath for index.js).
+
+// Create a file called ".env" in the root. Add LOCAL_DB=localhost to the dotenv file.
+
+// Test dotenv file is running when the index file runs. Use the instructions on the dotenv website but append .LOCAL_DB after "env".
+
+// Open the terminal. Check you are in the right directory (ls or .cd etc). Run npm (npm start). localhost should print to the console log.
