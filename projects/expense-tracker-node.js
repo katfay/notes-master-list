b5f776d05d185140f0c98e97c4ca5bf6415a19e7
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-06 13:24:35 +1100

    Project notes

--- projects/expense-tracker-node.js
+++ projects/expense-tracker-node.js
@@ -2,9 +2,6 @@
 // - create DB with collection with documents with data
 // - set up MongoDB queries (CRUD operations)
 
-
-
-
 // STEPS
 
 // //Create frontend shell (basic)
@@ -15,11 +12,19 @@
 // index.js file
 // .env file and add "DB_HOST:localhost" to line 1
 // In terminal, check node, npm, nodemon installed and versions
-// In terminal, run npm init to create JSON package 
+// In terminal, run npm init to create JSON package
 // In terminal, install express, body-parser, cors, mongoose, dotenv (npm install <name> --save)
 // Run npm intall
 // Add start script under test script and direct to relative index.js file ("nodemon index.js")
 // Add test script for dotenv from its npm website, append ".DB_HOST" after .env and test npm start to see if this console logs the correct result (to see if .env is being processed through node and index.js file)
 
-// // Add express functions to build local server and listen for connections
-// Create a 
+// // Add express functions to build local server and listen for connections (see node-express.js notes for detailed code)
+// In the index.js file, require express (assign as a const)
+// Assign app as a constant for express() [ const app = express() ]
+// Write a get request to handle reqeusts to the homepage ("/") i.e. show specified text on the homepage when "/" is accessed - see express notes
+// Write a listener request to listen for connections to the server - see express notes
+// Test that the local server has been created and is listening
+// Open the terminal and run npm start. If nodemon doesn't show errors, server should be listening
+// Check further by opening broswer, typing in localhost:<portnumber>
+// This is the port number you used in the express listen request
+// You should see in the body of the browser page, the contents of the index.js file express get request e.g. "Hello World" or whatever text/html you have included.
