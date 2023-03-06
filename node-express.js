3f54f4dcfcdd17697d707dc7406a47c118afdac0
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-06 16:53:02 +1100

    Express notes

--- node-express.js
+++ node-express.js
@@ -24,7 +24,44 @@
 
 // Middleware functions that return a promise will call the next function when they reject or throw an error
 
-// EXAMPLE LISTEN REQUEST
+// // EXAMPLE EXPRESS SETUP - structure
+// Require the express module/package
+const express = require("express");
+
+//
+const app = express();
+
+// Use body-parser if you want to change the response objects to JSON format
+// Require the body-parser module/package
+const bodyparser = require("body-parser");
+
+// Use cors if we want to access an API that is from another origin/domain e.g. frontend is on netlify and bakcend is on netlify - this is cross-origin. IF we don't add in cors, it will say we don't have access.
+// cross-origin-resource-s
+// Require the cors module/package
+var cors = require("cors");
+
+// Use body-parser and cors in the express functions
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors());
+
+// Add in the middleware, where we can add in the API endpoints ("/endpoint")
+app.get("/endpoint", (req, res) => {
+  res.send(
+    `Kat's expense dashboard. Here's the <a href="/expense/list">Database</a>`
+  );
+});
+
+// And then an app.listen is needed to keep checking if the server is connected or not
+// This example listens on port 3000
+app.listen(3000, () => console.log("Server started");
+
+// Note this example also had the following controller requirements/functions:
+
+const expenseController = require("./controller/expenseController");
+app.use("/expense", expenseController);
+
+// // EXAMPLE LISTEN REQUEST
 // This function uses 3000 as our port and verifies the port is active if we see the console.log message
 // Import the express module
 var express = require("express");
@@ -34,7 +71,7 @@ app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
 
-// EXAMPLE LISTEN REQUEST 2
+// // EXAMPLE LISTEN REQUEST 2
 // This function starts a server and listens on port 3000 for connections
 // Import the express module
 var express = require("express");
