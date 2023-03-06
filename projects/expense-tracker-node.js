014c6ae6a0ef249f163e46412e1232d4c4beb362
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-06 13:12:00 +1100

    Project notes

--- projects/expense-tracker-node.js
+++ projects/expense-tracker-node.js
@@ -1,12 +1,25 @@
-/* NEXT STEPS
-- create DB with collection with documents with data
-- set up MongoDB queries (CRUD operations)
-
-
-*/
-
-/* NOTES
+// NEXT STEPS
+// - create DB with collection with documents with data
+// - set up MongoDB queries (CRUD operations)
 
 
 
-*/
+
+// STEPS
+
+// //Create frontend shell (basic)
+
+// // Create node initial environment ("backend env for our frontend")
+
+// New workspace (folder)
+// index.js file
+// .env file and add "DB_HOST:localhost" to line 1
+// In terminal, check node, npm, nodemon installed and versions
+// In terminal, run npm init to create JSON package 
+// In terminal, install express, body-parser, cors, mongoose, dotenv (npm install <name> --save)
+// Run npm intall
+// Add start script under test script and direct to relative index.js file ("nodemon index.js")
+// Add test script for dotenv from its npm website, append ".DB_HOST" after .env and test npm start to see if this console logs the correct result (to see if .env is being processed through node and index.js file)
+
+// // Add express functions to build local server and listen for connections
+// Create a 
